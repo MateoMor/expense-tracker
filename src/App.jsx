29e42d895b7a623d0d82d8a813e4bfe0746ff689
Mateo <frisded555@gmail.com>
2023-06-15 16:35:53 -0500

    Container min size changed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     return (
         <GlobalProvider>
             <div className="bg-zinc-950 text-white min-h-screen flex justify-center py-10">
-                <div className="container mx-auto min-w-[230px] w-1/2">
+                <div className="container mx-auto min-w-[248px] w-1/2">
                     <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-2 flex-wrap flex-s">
                         <div className="basis-1 grow">
                             <h1 className="text-4xl font-bold">
